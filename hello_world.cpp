32e379b948ca774808b0f90eeb6284e45e1850c0
psychoyit <jbr0085@gmail.com>
2016-03-05 13:44:01 -0600

    Initial hello world program

---
+++ hello_world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void hello_world();
+
+int main()
+{
+	hello_world();
+
+	return 0;
+}
+
+void hello_world()
+{
+	cout << "Hello world!\n";
+
+	return;
+}
